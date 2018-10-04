55a2b5ea45a0b9e78ad2a3fd98d565cf0b3e0359
anhnt2899 <anhnt28995@gmail.com>
2018-10-04 13:16:25 +0300

    .
    Chapter7 commit
    
    ABC
    
    ABC

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_page#home'
-  get 'static_page#home', to: "static_page#home"
-  get '/help', to: "static_page#help"
-  get '/about', to: "static_page#about"
-  get '/contact', to: "static_page#contact"
-  get '/new', to: 'user#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  get "users/new"
+  root "static_page#home"
+  get "static_page#home", to: "static_page#home"
+  get "/help", to: "static_page#help"
+  get "/about", to: "static_page#about"
+  get "/contact", to: "static_page#contact"
+  get "/signup", to: "users#new"
+  post "/signup",  to: "users#create"
+  resources :users
 end
