0c6227be19470cf286d8f2e061abd27657b0abc6
theanh28995 <theanh.neonstudio@gmail.com>
2018-10-11 08:46:59 +0700

    Chapter10 commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,16 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = t ".danger"
       render :new
     end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
